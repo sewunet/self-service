e927c2af2a7854e87554ead8bda0cf01366e7e5c
Sewunet Abebaw <sewunet92@gmail.com>
2025-08-08 11:48:37 +0300

    Updated ProfileSection.tsx

--- components/profile/ProfileSection.tsx
+++ components/profile/ProfileSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { ChevronRight } from 'lucide-react-native';
-import { LucideIcon } from 'lucide-react-native';
+import { Video as LucideIcon } from 'lucide-react-native';
 
 interface ProfileSectionItem {
   title: string;
